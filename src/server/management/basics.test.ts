b5504fc8bf5f520d53b993106e8f3e29325828e8
Florian Rappl <rappl.florian@gmail.com>
2018-02-08 23:35:57 +0100

    Added some more tests

---
+++ src/server/management/basics.test.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'path';
+import { configOf } from './basics';
+
+describe('Management API - Basics', () => {
+  it('configOf should return a function', () => {
+    const handler = configOf(undefined, undefined);
+    expect(typeof handler).toBe('function');
+  });
+
+  it('configOf should read the version from the pkg json', () => {
+    const version = require(resolve(__dirname, '../../../package.json')).version;
+    const config: any = {};
+    const handler = configOf(undefined, config);
+    let result: any;
+    handler(undefined, {
+      json(obj: any) {
+        result = obj;
+      }
+    } as any);
+    expect(result).toHaveProperty('version', version);
+  });
+
+  it('configOf should have the directory and name from the config', () => {
+    const config: any = {
+      name: 'foo',
+      directory: 'bar',
+    };
+    const handler = configOf(undefined, config);
+    let result: any;
+    handler(undefined, {
+      json(obj: any) {
+        result = obj;
+      }
+    } as any);
+    expect(result).toHaveProperty('name', config.name);
+    expect(result).toHaveProperty('directory', config.directory);
+  });
+});
---
+++ src/server/management/broadcast.test.ts
@@ -0,0 +1,25 @@
+import { broadcastAt } from './broadcast';
+
+describe('Management API - Broadcast', () => {
+  it('broadcastAt should return a function', () => {
+    const handler = broadcastAt(undefined);
+    expect(typeof handler).toBe('function');
+  });
+
+  it('broadcastAt should call into the server', () => {
+    let result: any;
+    const server: any = {
+      broadcast(msg: any) {
+        result = msg;
+      }
+    };
+    const req: any = {
+      body: 'foo bar'
+    };
+    const handler = broadcastAt(server);
+    handler(req, {
+      sendStatus() {}
+    } as any)
+    expect(result).toBe(req.body);
+  });
+});
