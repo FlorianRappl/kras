182c021537b3cfa1be610c63ffeb47458fb867ab
Florian Rappl <rappl.florian@gmail.com>
2018-02-10 23:59:31 +0100

    Reordered

--- src/client/components/index.tsx
+++ src/client/components/index.tsx
@@ -1,7 +1,7 @@
 export { Page } from './page';
+export { Feed } from './feed';
 export { Panel } from './panel';
 export { Loader } from './loader';
 export { Spinner } from './spinner';
 export { Protect } from './protect';
 export { Details } from './details';
-export { Feed } from './feed';
