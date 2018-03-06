3b7ccfebc59245bf12e6cf7c5f873443c143429a
Florian Rappl <rappl.florian@gmail.com>
2018-03-06 19:39:07 +0100

    Improved the proxy injector and provide some mmore tests

---
+++ src/server/helpers/build-response.test.ts
@@ -0,0 +1,83 @@
+import { fromHar, fromJson, fromNode, fromMissing } from './build-response';
+
+describe('Build Response', () => {
+  describe('fromHar', () => {
+    it('is possible with an empty HAR response', () => {
+      const result = fromHar('foo', {
+        content: 'bar',
+        headers: [],
+      });
+      expect(result.url).toBe('foo');
+    });
+  });
+
+  describe('fromJson', () => {
+    it('transports status code', () => {
+      const result = fromJson('foo', 200, 'Foo', {}, '');
+      expect(result.url).toBe('foo');
+      expect(result.status).toEqual({
+        code: 200,
+        text: 'Foo',
+      });
+    });
+
+    it('works with content and has default status code', () => {
+      const result = fromJson('http://google.com', undefined, undefined, {}, 'Foobar');
+      expect(result.url).toBe('http://google.com');
+      expect(result.status).toEqual({
+        code: 200,
+        text: '',
+      });
+      expect(result.content).toBe('Foobar');
+    });
+  });
+
+  describe('fromNode', () => {
+    it('is possible with empty node response', () => {
+      const result = fromNode({
+        url: 'foo',
+        headers: {},
+      }, 'Foo');
+      expect(result.content).toBe('Foo');
+      expect(result.url).toBe('foo');
+      expect(result.status).toEqual({
+        code: 200,
+        text: '',
+      });
+    });
+
+    it('is handling full node response', () => {
+      const result = fromNode({
+        url: 'http://google.com',
+        headers: {
+          a: '5',
+          b: 'c',
+        },
+        statusCode: 400,
+      }, 'Foo');
+      expect(result.url).toBe('http://google.com');
+      expect(result.content).toBe('Foo');
+      expect(result.status).toEqual({
+        code: 400,
+        text: '',
+      });
+      expect(result.headers).toEqual({
+        'a': '5',
+        'b': 'c',
+        'content-type': 'text/html',
+      });
+    });
+  });
+
+  describe('fromMissing', () => {
+    it('is sufficient with a single URL', () => {
+      const result = fromMissing('foo');
+      expect(result.url).toBe('foo');
+    });
+
+    it('is passing through undefined URL to be empty', () => {
+      const result = fromMissing(undefined);
+      expect(result.url).toBe('');
+    });
+  });
+});
--- src/server/injectors/proxy-injector.ts
+++ src/server/injectors/proxy-injector.ts
@@ -37,11 +37,11 @@ export default class ProxyInjector implements KrasInjector {
       });
       ws.on('message', data => {
         core.emit('message', { content: data, from: url, to: e.id });
-        e.ws.send(data);
+        e.ws.send(data, (err: Error) => core.emit('error', err));
       });
       e.ws.on('message', (data: WebSocket.Data) => {
         core.emit('message', { content: data, to: url, from: e.id });
-        ws.send(data);
+        ws.send(data, (err: Error) => core.emit('error', err));
       });
       ws.on('error', err => core.emit('error', err.error));
       this.sessions[e.id] = ws;
