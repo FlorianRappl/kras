00fe1eb1b1ae9000a16e8b5eb1cc0af84a8d58f9
amu <carvin_lo@163.com>
2022-08-26 21:26:15 +0800

    refactor: read cert from node_modules

--- src/server/core/readSsl.ts
+++ src/server/core/readSsl.ts
@@ -1,11 +1,20 @@
 import { readFileSync } from 'fs';
 import { SslConfiguration } from '../types';
 
+function fileReader(path: string) {
+  try {
+    return readFileSync(path);
+  } catch (e) {
+    // try to read file from node_modules
+    return readFileSync(require.resolve(path));
+  }
+}
+
 export function readSsl(config?: Partial<SslConfiguration>) {
   if (typeof config === 'object' && config.key && config.cert) {
     return {
-      key: readFileSync(config.key),
-      cert: readFileSync(config.cert),
+      key: fileReader(config.key),
+      cert: fileReader(config.cert),
     };
   }
 }
